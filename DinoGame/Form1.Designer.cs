6bca157feb45f7077a310b1507caf9d75ba20567
Stephen-Sim <simshansiong2002@gmail.com>
2024-02-13 22:19:11 +0800

    Add project files.

---
+++ DinoGame/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace DinoGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pointLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.FlowerPictureBox = new System.Windows.Forms.PictureBox();
+            this.DinoPictureBox = new System.Windows.Forms.PictureBox();
+            this.sandPcitureBox = new System.Windows.Forms.PictureBox();
+            this.gamOverPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.restartBtn = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.FlowerPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DinoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sandPcitureBox)).BeginInit();
+            this.gamOverPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // pointLabel
+            // 
+            resources.ApplyResources(this.pointLabel, "pointLabel");
+            this.pointLabel.Name = "pointLabel";
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // FlowerPictureBox
+            // 
+            this.FlowerPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.FlowerPictureBox.Image = global::DinoGame.Properties.Resources.iris;
+            resources.ApplyResources(this.FlowerPictureBox, "FlowerPictureBox");
+            this.FlowerPictureBox.Name = "FlowerPictureBox";
+            this.FlowerPictureBox.TabStop = false;
+            // 
+            // DinoPictureBox
+            // 
+            this.DinoPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.DinoPictureBox.Image = global::DinoGame.Properties.Resources.DinoStart;
+            resources.ApplyResources(this.DinoPictureBox, "DinoPictureBox");
+            this.DinoPictureBox.Name = "DinoPictureBox";
+            this.DinoPictureBox.TabStop = false;
+            // 
+            // sandPcitureBox
+            // 
+            resources.ApplyResources(this.sandPcitureBox, "sandPcitureBox");
+            this.sandPcitureBox.Name = "sandPcitureBox";
+            this.sandPcitureBox.TabStop = false;
+            // 
+            // gamOverPanel
+            // 
+            this.gamOverPanel.Controls.Add(this.scoreLabel);
+            this.gamOverPanel.Controls.Add(this.restartBtn);
+            this.gamOverPanel.Controls.Add(this.label1);
+            resources.ApplyResources(this.gamOverPanel, "gamOverPanel");
+            this.gamOverPanel.Name = "gamOverPanel";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // restartBtn
+            // 
+            resources.ApplyResources(this.restartBtn, "restartBtn");
+            this.restartBtn.Name = "restartBtn";
+            this.restartBtn.UseVisualStyleBackColor = true;
+            this.restartBtn.Click += new System.EventHandler(this.restartBtn_Click);
+            // 
+            // scoreLabel
+            // 
+            resources.ApplyResources(this.scoreLabel, "scoreLabel");
+            this.scoreLabel.Name = "scoreLabel";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.gamOverPanel);
+            this.Controls.Add(this.FlowerPictureBox);
+            this.Controls.Add(this.DinoPictureBox);
+            this.Controls.Add(this.sandPcitureBox);
+            this.Controls.Add(this.pointLabel);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.FlowerPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DinoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sandPcitureBox)).EndInit();
+            this.gamOverPanel.ResumeLayout(false);
+            this.gamOverPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox sandPcitureBox;
+        private System.Windows.Forms.PictureBox DinoPictureBox;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label pointLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox FlowerPictureBox;
+        private System.Windows.Forms.Panel gamOverPanel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Button restartBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DinoGame/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinoGame
+{
+    public partial class Form1 : Form
+    {
+        public Point InitialPoint { get; set; }
+        public Point FlowerInitialPoint { get; set; }
+        private bool isSapcePressed = false;
+        private int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FlowerInitialPoint = FlowerPictureBox.Location;
+            InitialPoint = DinoPictureBox.Location;
+
+            gamOverPanel.Visible = false;
+        }
+
+        async void DinoGamePlay()
+        {
+            counter++;
+            pointLabel.Text = (counter * 5).ToString("00000");
+
+            if (isSapcePressed)
+            {
+                DinoPictureBox.Image = Properties.Resources.DinoStart;
+                DinoPictureBox.Location = new Point(InitialPoint.X, InitialPoint.Y - 100);
+
+                await Task.Delay(900);
+
+                isSapcePressed = false;
+            }
+            else if (counter % 2 == 0)
+            {
+                DinoPictureBox.Image = Properties.Resources.DinoRun1;
+                DinoPictureBox.Location = InitialPoint;
+            }
+            else
+            {
+                DinoPictureBox.Image = Properties.Resources.DinoRun2;
+                DinoPictureBox.Location = InitialPoint;
+            }
+        }
+
+        void SandMove()
+        {
+            if (sandPcitureBox.Location.X <= -300)
+            {
+                sandPcitureBox.Location = new Point(0, sandPcitureBox.Location.Y);
+            }
+            else
+            {
+                sandPcitureBox.Location = new Point(sandPcitureBox.Location.X - 20, sandPcitureBox.Location.Y);
+            }
+        }
+
+        void FlowerMove()
+        {
+            if (counter * 5 % 50 == 0 && FlowerPictureBox.Location.X <= 0)
+            {
+                FlowerPictureBox.Location = new Point(800, FlowerPictureBox.Location.Y);
+            }
+            else
+            {
+                FlowerPictureBox.Location = new Point(FlowerPictureBox.Location.X - 20, FlowerPictureBox.Location.Y);
+            }
+        }
+
+        void CheckGameOver()
+        {
+            if (DinoPictureBox.Bounds.IntersectsWith(FlowerPictureBox.Bounds))
+            {
+                gameTimer.Stop();
+                scoreLabel.Text = $"Score: {counter * 5}";
+                this.gamOverPanel.Visible = true;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            DinoGamePlay();
+            SandMove();
+            FlowerMove();
+            CheckGameOver();
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            gameTimer.Interval = 100;
+            gameTimer.Start();
+            this.Focus();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                isSapcePressed = true;
+            }
+        }
+
+        private void restartBtn_Click(object sender, EventArgs e)
+        {
+            this.gamOverPanel.Visible = false;
+            counter = 0;
+            DinoPictureBox.Location = InitialPoint;
+            FlowerPictureBox.Location = FlowerInitialPoint;
+            isSapcePressed = false;
+            gameTimer.Interval = 100;
+            gameTimer.Start();
+            this.Focus();
+        }
+    }
+}
